e45a8b5fbe675ea52a4bcdfec86a3b3474ee865a
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-09-13 11:33:40 +0700

    feat: style page & add shop page

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 # create urls routing for the main app
 from django.urls import path
-from main.views import show_homepage
+from main.views import show_homepage,show_shop
 
 app_name = "main"
 
 urlpatterns = [
     path('', show_homepage, name='show_homepage'),
+    path('shop/', show_shop, name='show_shop'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def show_homepage(request):
     context = {
-        'app_name': "Aci Rezeki Shop",
         'author_name': "Ilham Abdillah Alhamdi",
         'class': "PBP B"
     }
     return render(request, "index.html", context)
+
+def show_shop(request):
+    context = {}
+    return render(request, "shop.html", context)
