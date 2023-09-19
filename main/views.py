da780b8d21e2c442f16f758a1fff8caa230a7d9c
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-09-20 03:16:06 +0700

    feat: finishing assignment 3

--- main/views.py
+++ main/views.py
@@ -4,15 +4,19 @@ from django.http import HttpResponseRedirect, HttpResponse
 from main.models import Category, Item
 from main.forms import ItemForm, CategoryForm
 
-# Create your views here.
 def show_homepage(request):
     items = Item.objects.all()
     context = {
-        'author_name': "Ilham Abdillah Alhamdi",
-        'class': "PBP B",
         'items': items,
     }
     return render(request, "index.html", context)
+
+def show_about(request):
+    context = {
+        'author_name': "Ilham Abdillah Alhamdi",
+        'class': "PBP B"
+    }
+    return render(request, "about.html", context)
 
 def create_item(request):
     form = ItemForm(request.POST or None)
@@ -25,19 +29,25 @@ def create_item(request):
     context = {'form': form}
     return render(request, 'create_item.html', context)
 
-
 def show_json(request):
     data = Item.objects.all()
-    HttpResponse(serializers.serialize('json', data), content_type="application/json")
+    return HttpResponse(serializers.serialize('json', data), content_type="application/json")
 
 def show_xml(request):
     data = Item.objects.all()
-    HttpResponse(serializers.serialize('xml', data), content_type="application/xml")
+    return HttpResponse(serializers.serialize('xml', data), content_type="application/xml")
 
 def show_json_by_id(request, id):
     data = Item.objects.filter(id=id)
-    HttpResponse(serializers.serialize('json', data), content_type="application/json")
+    return HttpResponse(serializers.serialize('json', data), content_type="application/json")
 
 def show_xml_by_id(request, id):
     data = Item.objects.filter(id=id)
-    HttpResponse(serializers.serialize('xml', data), content_type="application/xml")
+    return HttpResponse(serializers.serialize('xml', data), content_type="application/xml")
+
+def show_html_by_id(request, id):
+    data = Item.objects.get(id=id)
+    context = {
+        'data': data,
+    }
+    return render(request, "item_by_id.html", context)
