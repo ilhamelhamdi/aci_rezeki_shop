82ac4441a587be61cc44c8c5e5977afadb5b0e27
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-11-22 12:40:00 +0700

    Integration with Flutter (auth & item)

--- main/urls.py
+++ main/urls.py
@@ -1,22 +1,25 @@
 # create urls routing for the main app
 from django.urls import path
-from main.views import show_homepage, create_item,create_item_ajax, get_items_json, get_items_xml, get_item_by_id_json,get_item_by_id_xml, show_about, show_item_detail, register, login_user, logout_user, update_item, delete_item
+import main.views as views
 
 app_name = "main"
 
 urlpatterns = [
-    path('', show_homepage, name='show_homepage'),
-    path('about/', show_about, name='show_about'),
-    path('create-item/', create_item, name='create_item'),
-    path('create-ajax/', create_item_ajax, name='create_item_ajax'),
-    path('json/', get_items_json, name='get_items_json'),
-    path('xml/', get_items_xml, name='get_items_xml'),
-    path('json/<int:id>/', get_item_by_id_json, name='get_item_by_id_json'),
-    path('xml/<int:id>/', get_item_by_id_xml, name='get_item_by_id_xml'),
-    path('item/<int:id>/', show_item_detail, name='show_item_detail'),
-    path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
-    path('update-item/<int:id>/', update_item, name='update_item'),
-    path('delete-item/<int:id>/', delete_item, name='delete_item'),
+    path('', views.show_homepage, name='show_homepage'),
+    path('about/', views.show_about, name='show_about'),
+    path('create-item/', views.create_item, name='create_item'),
+    path('create-ajax/', views.create_item_ajax, name='create_item_ajax'),
+    path('json/', views.get_items_json, name='get_items_json'),
+    path('xml/', views.get_items_xml, name='get_items_xml'),
+    path('json/<int:id>/', views.get_item_by_id_json, name='get_item_by_id_json'),
+    path('xml/<int:id>/', views.get_item_by_id_xml, name='get_item_by_id_xml'),
+    path('item/<int:id>/', views.show_item_detail, name='show_item_detail'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('update-item/<int:id>/', views.update_item, name='update_item'),
+    path('delete-item/<int:id>/', views.delete_item, name='delete_item'),
+    path('api/login/', views.login, name='login_api'),
+    path('api/logout/', views.logout, name='logout_api'),
+    path('api/register/', views.register, name='register_api'),
 ]
